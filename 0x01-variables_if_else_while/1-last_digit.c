cedb619dbfcad8e16ab75d4c86c4206c4373a7dc
j-grace1 <nchindejoshoua@gmail.com>
2022-06-04 09:09:34 +0100

    correcting my algo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,22 +9,23 @@
  */
 int main(void)
 {
-	int n;
+	int n, digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", digit);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", digit);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", digit);
 	}
 
 	return (0);
