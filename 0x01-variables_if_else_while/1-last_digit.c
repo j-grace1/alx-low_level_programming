1f037c7583e4f8aa0b744d42786dbb1ff52f455e
j-grace1 <nchindejoshoua@gmail.com>
2022-06-04 09:31:07 +0100

    removed the abs funtion in the print statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, abs(digit));
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
 	else if (n < 6 && n != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, abs(digit));
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, abs(digit));
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
 
 	return (0);
