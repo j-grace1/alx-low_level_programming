a873774794320f500c4800cefe500380752119a5
j-grace1 <nchindejoshoua@gmail.com>
2022-06-04 11:16:11 +0100

    #include <stdio.h>

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always successful
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
