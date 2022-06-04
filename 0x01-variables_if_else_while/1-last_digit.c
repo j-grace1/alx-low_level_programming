a78df9679569c72ff0b6f66d003ab2a47694c912
j-grace1 <nchindejoshoua@gmail.com>
2022-06-04 09:38:45 +0100

    corrected some stuff

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints if a number is greater than 5,less than 6 and not 0 or equals to 0
+ * main - Prints if a numb is > 5,< 6 and not 0 or = 0
  *
- * Return: Always successfull
+ * Return: Always successful
  */
 int main(void)
 {
@@ -19,7 +19,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
-	else if (digit < 6 && n != 0)
+	else if (digit < 6 && digit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	}
