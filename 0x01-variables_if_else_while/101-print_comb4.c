4722b17b1934a5512858d3936348eef6de8347f8
j-grace1 <nchindejoshoua@gmail.com>
2022-06-04 13:07:07 +0100

    Prints numbers from 012 to 789 with commas and spaces in such a way that no most significant digit is greater athan the least significant digit after it at any pont in time

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+  * main - Prints numbers from 012 to 789 with commas and spaces
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int num, a, b;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		for (a = '0'; a <= '9'; a++)
+		{
+            for (b = '0'; b <= '9'; b++)
+		    {
+                if (num < a && a < b)
+			    {
+				    putchar(num);
+				    putchar(a);
+                    putchar(b);
+
+				    if (num != '8' || (num == '8' && a != '9'))
+				    {
+					    putchar(',');
+					    putchar(' ');
+				    }
+			    }
+            }
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
