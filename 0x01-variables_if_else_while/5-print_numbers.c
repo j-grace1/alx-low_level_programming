c8c7a00d53317aa0c7c22e688d3d93cd7c8b0ab5
j-grace1 <nchindejoshoua@gmail.com>
2022-06-04 11:29:19 +0100

    this file prints decimal numberes from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the decimal numbers from 0 to 9
+ *
+ * Return: Always successful
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = "0"; num <= "0"; num++)
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
+}
