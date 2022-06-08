630931162b524fb810994d77dada3ab69014c456
j-grace1 <nchindejoshoua@gmail.com>
2022-06-08 11:53:11 +0100

    included main.c header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,13 @@
-#include <studio.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
 * main- prints _putschar
-*_putchar - writes the character c to stdout
-* @c: The character to print
 *
 * Return: Always successful
 */
 
 int main()
 {
-
     _putchar("_putschar");
     
     return (0);
