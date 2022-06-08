c870d88e4e7cbc1f618b1fb233d1dd0b0da9ae15
j-grace1 <nchindejoshoua@gmail.com>
2022-06-08 12:00:35 +0100

    edited my coode

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,16 @@
 * Return: Always successful
 */
 
-int main()
+int main(void)
 {
-    _putchar("_putschar");
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
     
     return (0);
 }
