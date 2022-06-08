1fe683bdfe53cea8e85df7571da8ecb782f761bd
j-grace1 <nchindejoshoua@gmail.com>
2022-06-08 11:47:33 +0100

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+#include <unistd.h>
+
+/**
+* main- prints _putschar
+*_putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: Always successful
+*/
+
+int main()
+{
+
+    _putchar("_putschar");
+    
+    return (0);
+}
