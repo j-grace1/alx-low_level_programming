7ff28356417572c8b3af8abfee34a615c051a2c1
j-grace1 <nchindejoshoua@gmail.com>
2022-06-08 19:30:53 +0100

    alphabet-10x prints the lower case alphaber 10 time

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - alphabet in lowercase 10 times
+ *
+ * Return: returns 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+char i;
+int a;
+
+for ( a = 0; i < 11; i++)
+{
+    for (i = 'a'; i <= 'z'; i++)
+    {
+	    _putchar(i);
+    }
+}
+}
+
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet in lowercase 10 times
+ *
+ * Return: returns 0
+ *
+ */
+void print_alphabet(void)
+{
