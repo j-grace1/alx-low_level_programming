2b9ce6d36260c0c8e4948397a68851925b68e0ce
j-grace1 <nchindejoshoua@gmail.com>
2022-06-08 21:17:15 +0100

    some cahnge

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,3 +8,18 @@
  */
 int _islower(int c)
 {
+    
+char i;
+    for (i = 0; i < 26; i++)
+    {
+        if (i == c)
+        {
+            _putchar('1');
+        }
+        else
+        {
+            _putchar('0');
+        }
+    }
+return(0); 
+}
