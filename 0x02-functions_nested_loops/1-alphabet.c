11565326d259cb7bdd94347721ca1ab773727626
j-grace1 <nchindejoshoua@gmail.com>
2022-06-08 15:04:25 +0100

    betty compliance

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,12 @@
  */
 void print_alphabet(void)
 {
-	char i;
+char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
+for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
