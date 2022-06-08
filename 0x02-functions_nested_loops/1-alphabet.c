158542600b40addc235d27bac4cfc94cba6850c4
j-grace1 <nchindejoshoua@gmail.com>
2022-06-08 14:59:19 +0100

    betty compliance

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,18 @@
 
 /**
  * main - alphabet in lowercase
- * 
+ *
  * Return: returns 0
- * 
+ *
  */
 void print_alphabet(void)
-    {
-    char i;
+{
+char i;
 
     for(i = 'a'; i <= 'z'; i++)
         {
             _putchar(i);
         }
 
-    _putchar('\n');
-    }
+_putchar('\n');
+}
