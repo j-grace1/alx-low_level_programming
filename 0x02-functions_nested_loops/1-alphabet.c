db2f6dad6d0a685eadf0f6ef69d8eb0914e2bbc6
j-grace1 <nchindejoshoua@gmail.com>
2022-06-08 14:52:14 +0100

    this added a c file thar prints lower case letters of the alphabet from a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - alphabet in lowercase
+ * 
+ * Return: returns 0
+ * 
+ */
+void print_alphabet(void)
+    {
+    for(char i = 'a'; i <= 'z'; i++)
+        {
+            _putchar(i);
+        }
+
+    _putchar('\n');
+    }
