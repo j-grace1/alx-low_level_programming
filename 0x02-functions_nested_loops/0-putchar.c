40b3e4f83b6e078190bc4033975fca49076b835a
j-grace1 <nchindejoshoua@gmail.com>
2022-06-08 14:05:39 +0100

    betty compliance

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-* main- prints _putschar
-*
-* Return: Always successful
-*/
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
     _putchar('_');
