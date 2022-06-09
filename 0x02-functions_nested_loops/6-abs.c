1a8fab9f4b91e28150d4820665a48f4b2ce9e2e0
j-grace1 <nchindejoshoua@gmail.com>
2022-06-09 03:41:55 +0100

    acs computes absolute valze of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _abs - compustes the absolute value of a funtion
+  * @n: The number that is to be checked
+  *
+  * Return: 1 for positive, -1 for negative else 0
+  */
+int _abs(int h)
+{
+	if (h < 0)
+	{
+		int absolute_value;
+
+		absolute_value = h / -1;
+
+		return (absolute_value);
+	}
+
+	return (h);
+}
