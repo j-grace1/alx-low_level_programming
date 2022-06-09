73a5778d3f9109290efe73f12337c958edd5361b
j-grace1 <nchindejoshoua@gmail.com>
2022-06-09 15:30:28 +0100

    this file prints the sum of tẃo given numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two numbers
+  * @a: The first number 
+  * @b: The second number 
+  *
+  * Return: Integer value
+  */
+int add(int b, int a)
+{
+	return (a + b);
+}
