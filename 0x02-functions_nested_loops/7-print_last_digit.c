4fed1ca1e4461f7669047d1dd8420b7c3f91ca15
j-grace1 <nchindejoshoua@gmail.com>
2022-06-09 03:55:01 +0100

    new files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_last_digit - Prints the last digit of a number
+  * @k: The number to be checked
+  *
+  * Return: Value of the last digit of number
+  */
+int print_last_digit(int k)
+{
+	int last_digit;
+
+	last_digit = k % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit = last_digit / -1;
+	}
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * jack_bauer - Prints every minute of the day 
+  *
+  * Return: .........
+  */
+void jack_bauer(void)
+{
+	int i, x, y, z;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			if ((y <= 1 && y <= 9) || (i <= 2 && x <= 3))
+			{
+				for (y = 0; y <= 5; y++)
+				{
+					for (z = 0; z <= 9; z++)
+					{
+						_putchar(i + '0');
+						_putchar(x + '0');
+						_putchar(58);
+						_putchar(y + '0');
+						_putchar(z + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
