a09dc99ffd2d4cff703f5b059a9511c0c58fe62a
j-grace1 <nchindejoshoua@gmail.com>
2022-06-09 22:58:37 +0100

    little change

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the first 98 fibonacci numbers
+  * main - Prints the first 50 fibonacci numbers
   *
   * Return: Nothing
   */
-int main()
+int main(void)
 {
 	int i = 0;
 	long j = 1, k = 2;
