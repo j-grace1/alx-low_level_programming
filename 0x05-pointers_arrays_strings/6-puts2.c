bc5fbd723f391f73fc63ea9f337afdc709f0cb47
j-grace1 <nchindejoshoua@gmail.com>
2022-06-15 17:42:23 +0100

    prints eeach character in a sting followed by new line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string 
+ * followed by a new line
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+        _putchar('\n');
+	}
+}
