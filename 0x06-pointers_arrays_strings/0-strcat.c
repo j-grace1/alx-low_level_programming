b7c234b6753962dcb928bcd5fcf562ce46875713
j-grace1 <nchindejoshoua@gmail.com>
2022-06-16 22:41:18 +0100

    concatednates two wtrings pointed by the inputs overriding thier the null of the first

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,9 +8,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
+    int i = 0, length1 = 0, length2 = 0, j = 0;
+	char element1 = dest[0], element2 = src[0];
+
+	while (element1 != '\0')
+	{
+		length1++;
+		element1 = dest[i++];
+	}
+
+    while (element2 != '\0')
+	{
+		length2++;
+		element2 = src[i++];
+	}
   
-    char *ptr;
-    
-    * ptr = *dest + *src;
-    return(ptr);
+	for (; i <= element2 - 1; i++)
+	{
+		dest[(element1 - 1) + i] = src[i];
+	}
+	return (dest);
 }
