b76bb3b309214a24a97af59d912c079427eafe27
j-grace1 <nchindejoshoua@gmail.com>
2022-06-16 18:31:31 +0100

    new project

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char main(char *dest, char *src)
+{
+    dest = "good";
+    src = "foot";
+   unsigned int i;
+    char *ptr;
+    int len = strlen(dest);
+    for( i = 0; i < (strlen(dest) + strlen(src)); i++)
+    {
+        if(dest[i] == '\0')
+        {
+            for(unsigned int j = 0; j < strlen(src); j++)
+            {
+                dest[len + j] = src[j];
+            }
+            break;
+        }
+    }
+}
