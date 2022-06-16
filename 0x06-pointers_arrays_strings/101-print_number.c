f443cdb6ade2a007a87c7f97e7656a09d63b3b6a
j-grace1 <nchindejoshoua@gmail.com>
2022-06-17 00:22:48 +0100

    prin./c.bat

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
+ */
+void print_number(int n)
+{
+	_putchar(n);
+    _putchar("\n");
+}
