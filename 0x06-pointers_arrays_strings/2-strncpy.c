956f9d408078d59bce70b4c8e9033a8ab69b41a0
j-grace1 <nchindejoshoua@gmail.com>
2022-06-16 23:39:58 +0100

    conacatenares rwo strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest:copy to
+ * @src: copy from
+ * @n: n
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+	}
+
+	for (b = 0; b < n; b++)
+	{
+		dest[a + b] = src[b];
+		if (src[b] == '\0')
+			b = n;
+	}
+	return (dest);
+}
