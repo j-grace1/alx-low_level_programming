3c7713a3a55f9d440fb04959f68b19fcc8e962bc
j-grace1 <nchindejoshoua@gmail.com>
2022-07-20 19:08:06 +0100

    new project on more pointers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_array - prints n elements of an array
- * @a: array name
- * @n: is the number of elements OF the array to be printed
+ * print_array - prints integer
+ * @n: interger to be printed
  * Return: a and n inputs
  */
 void print_number(int n)
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-0x00-hello_world
-0x00-hello_world
+0x08-recursion
