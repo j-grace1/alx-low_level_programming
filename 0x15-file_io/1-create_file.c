b71b8de0720aa9c307d1f5bc5a778b9774bfca64
j-grace1 <nchindejoshoua@gmail.com>
2022-07-26 20:43:23 +0100

    some little change in code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 
     if (text_content != NULL)
     {
-        for (writecount = 0, ptr = text_content; *ptr; *ptr++)
+        for (writecount = 0, ptr = text_content; *ptr; ptr++)
         {
             writecount++;
         }
