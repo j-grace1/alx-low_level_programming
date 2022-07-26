ee6b1a766662aa6f91c0b0bc026e8a09b0ba4747
j-grace1 <nchindejoshoua@gmail.com>
2022-07-26 20:24:14 +0100

    code modification

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,8 @@
 int create_file(const char *filename, char *text_content)
 {
     int fd;
-    int written;
+    int written, writecount;
+    char *ptr;
 
     if (filename == NULL)
     {
@@ -25,10 +26,18 @@ int create_file(const char *filename, char *text_content)
     {
         return(-1);
     }
-    else
+    if (text_content != NULL)
     {
-        return(1);
+        for (writecount = 0; *text_content; *text_content++)
+        {
+            writecount++;
+            written = write(fd, text_content, writecount);
+        }
     }
-    written = write(fd, text_content, sizeof(text_content));
     close(fd);
+    if (written != writecount || close(fd) == -1)
+    {
+        return(-1);
+    }
+    return(1);
 }
