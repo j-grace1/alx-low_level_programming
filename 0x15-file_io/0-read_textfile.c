def9806bd94e6352a40cd7cd32ab27e090e00998
j-grace1 <nchindejoshoua@gmail.com>
2022-07-26 18:01:17 +0100

    modifying read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,26 +1,46 @@
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - prints text from a file
+ *
+ * @filename: name of the file
+ * @letters: number of characters to read
+ *
+ * Return: actual number of letters read, 0 if end of file
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int fd;
     char buffer[100];
+    int written;
+    
+    buffer = malloc(sizeof(char)*letters);
+    if(filename == NULL| letters = 0){
+        return(0);
+    }
+    if(buffer == NULL){
+        return(0);
+    }
     fd = open(filename, O_RDONLY);
     if(fd == -1){
+        free(buf)
         return(0);
     }
-    if(filename == NULL){
+    lseek(fd, 0, SEEK_SET);
+    read(fd, buffer, letters);
+    if (read(fd, buffer, letters) == -1)
+    {
         return(0);
-    }
-    if(fd != -1){
-        lseek(fd, 0, SEEK_SET);
-        read(fd, buffer, letters);
-        _putchar(buffer);
-        return(sizeof(buffer));
         close(fd);
-    }  
+    }
+    written = write(STDOUT_FILENO, buffer, letters);
+    if (written != letters)
+    {
+        return(0);
+    }
+    return(letters);
+    close(fd); 
 }
