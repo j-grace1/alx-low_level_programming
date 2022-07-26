111e5f5c946392f450c8e3a4e97f137aaaed14e6
j-grace1 <nchindejoshoua@gmail.com>
2022-07-26 20:41:12 +0100

    little adjustments

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@
 int create_file(const char *filename, char *text_content)
 {
     int fd;
-    int written, writecount;
+    int written = 0, writecount = 0;
     char *ptr;
 
     if (filename == NULL)
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
         return(-1);
     }
     
-    fd = open(filename, O_RDWR|O_EXCL|O_TRUNC, 0600);
+    fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0600);
     if (fd == -1)
     {
         return(-1);
