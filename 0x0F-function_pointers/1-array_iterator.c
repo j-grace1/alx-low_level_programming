2f978a5f603d87071e5f9cf77c968213566eada3
j-grace1 <nchindejoshoua@gmail.com>
2022-08-01 21:49:43 +0100

    array_iterator takes array pointer and and fuction pointer and passes element of array to fuction

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_elem - prints an integer
+ * @array: array of integers of parameters
+ * @size: size of the array
+ * @action: function to be executed
+ * using array parameters
+ * 
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+	int i = 0;
+	while (i =! \0)
+	{
+		action(array[i]);
+		i++;
+	} 
+}
