4fad8c22a9bfe36034eaceb36da5f42769f3e390
j-grace1 <nchindejoshoua@gmail.com>
2022-08-01 21:52:16 +0100

    debbuged code

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 	int i = 0;
-	while (i =! \0)
+	while (i != \0)
 	{
 		action(array[i]);
 		i++;
