065c6e5237863c8273d5cdcea6839233b67c0fa8
j-grace1 <nchindejoshoua@gmail.com>
2022-08-01 23:00:08 +0100

    betty compliance

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,6 +3,8 @@
 
 /**
  * print_elem - prints an integer
+ * @array_iterator: iterates through an array
+ * and passes elements to a function pointer
  * @array: array of integers of parameters
  * @size: size of the array
  * @action: function to be executed
@@ -14,9 +16,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 	if (array == NULL || action == NULL)
+
 		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
-	} 
+	}
 }
