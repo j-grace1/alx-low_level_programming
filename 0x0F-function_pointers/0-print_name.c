19785ab50a792eafc1e61e772bfe1ade9db29e04
j-grace1 <nchindejoshoua@gmail.com>
2022-08-01 21:07:26 +0100

    some modification in code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,23 +2,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_as_is(char *name)
-{
-_putchar(*name);
-}
-
-/**
  * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: fuction to print name
  * Return: Nothing.
  */
-void print_name(char *name, void (*f)(char *)){
-f = &print_name_as_is;
-*f(name);
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return(0);
+	*f(name);
 }
+
