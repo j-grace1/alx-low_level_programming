a6cb25c04801b7db2b4589213062a1a347d6a787
j-grace1 <nchidejoshoua@gmail.com>
2023-03-11 15:01:11 -0500

    modified 3-pirnt_all.c file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,58 +1,96 @@
-#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_char - Prints a char.
- * @list: A va_list containing the char to be printed.
- */
-void print_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-
-/**
- * print_int - Prints an int.
- * @list: A va_list containing the int to be printed.
- */
-void print_int(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-
-/**
- * print_float - Prints a float.
- * @list: A va_list containing the float to be printed.
- */
-void print_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-/**
- * print_string - Prints a string.
- * @list: A va_list containing the string to be printed.
- */
-void print_string(va_list list)
-{
-	char *str = va_arg(list, char *);
-
-	if (str == NULL)
-	{
-		str = "(nil)";
-	}
-
-	printf("%s", str);
-}
-
-/**
- * print_all - Prints anything, followed by a new line.
- * @format: A list of types of arguments passed to the function.
+ * print_char - Prints a character
  *
- * Return: void
+ * @valist: A va_list of arguments
+ */
+void print_char(va_list valist)
+{
+    printf("%c", va_arg(valist, int));
+}
+
+/**
+ * print_int - Prints an integer
+ *
+ * @valist: A va_list of arguments
+ */
+void print_int(va_list valist)
+{
+    printf("%d", va_arg(valist, int));
+}
+
+/**
+ * print_float - Prints a float
+ *
+ * @valist: A va_list of arguments
+ */
+void print_float(va_list valist)
+{
+    printf("%f", va_arg(valist, double));
+}
+
+/**
+ * print_string - Prints a string
+ *
+ * @valist: A va_list of arguments
+ */
+void print_string(va_list valist)
+{
+    char *s = va_arg(valist, char *);
+
+    if (s == NULL)
+    {
+        printf("(nil)");
+        return;
+    }
+
+    printf("%s", s);
+}
+
+/**
+ * print_all - Prints anything
+ *
+ * @format: A list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	va_list list;
-	unsigned int i = 0, j;
-	char *separator = "";
+    va_list valist;
+    int i = 0, j = 0;
+    char *sep = "";
 
-	op_t ops[]
+    print_fn print_arr[] = {
+        {'c', print_char},
+        {'i', print_int},
+        {'f', print_float},
+        {'s', print_string},
+        {0, NULL}
+    };
+
+    va_start(valist, format);
+
+    while (format && format[i])
+    {
+        j = 0;
+
+        while (print_arr[j].type != 0)
+        {
+            if (format[i] == print_arr[j].type)
+            {
+                printf("%s", sep);
+                print_arr[j].func(valist);
+                sep = ", ";
+                break;
+            }
+
+            j++;
+        }
+
+        i++;
+    }
+
+    va_end(valist);
+
+    printf("\n");
+}
